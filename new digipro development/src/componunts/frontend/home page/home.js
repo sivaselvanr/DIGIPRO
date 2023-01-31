0f64e7071a1bbda9b75b8c50510f66460388c794
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 11:25:22 +0530

    siva Tuesday 31 January 2023 11:25:22 AM IST

--- new digipro development/src/componunts/frontend/home page/home.js
+++ new digipro development/src/componunts/frontend/home page/home.js
@@ -13,7 +13,7 @@ import Choose_role from "../choose role/choose role";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //importing student_page
-import 
+import Studen
 
 //Home function
 function Home() {
