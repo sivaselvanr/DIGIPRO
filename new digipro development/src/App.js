b970627afb8da62e5773d7aac9a0a33801c434dd
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 11:47:29 +0530

    siva Tuesday 31 January 2023 11:47:29 AM IST

--- new digipro development/src/App.js
+++ new digipro development/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
           {/* browser url = http://hostname:port */}
           <Route path="/" element={<Home />} />
-          <Route pat
+          <Route path=''
         </Routes>
       </div>
     );
