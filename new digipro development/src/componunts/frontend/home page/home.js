cde243988cac70baf0c35faed4b9bf51fc226edd
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 12:04:09 +0530

    siva Tuesday 31 January 2023 12:04:09 PM IST

--- new digipro development/src/componunts/frontend/home page/home.js
+++ new digipro development/src/componunts/frontend/home page/home.js
@@ -88,7 +88,7 @@ After some additional, small investments through the end of 1998 to early 1999,[
 }
 export default Home;
 
-function chan() {
+function routeChange() {
     return (
       <div>
         <Routes>
