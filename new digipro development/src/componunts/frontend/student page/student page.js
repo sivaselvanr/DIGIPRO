0ebe34b75e57a518943b6fa4c9a0a1cf968cc24f
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 20:04:35 +0530

    siva Tuesday 31 January 2023 08:04:35 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -27,10 +27,10 @@ export default function Student_page() {
                 <form>
 
                     {/* search input field and search button to search student details*/}
-                    <div></div><p className="search_field">
+                    <div><p className="search_field">
                         <input type="search" name="search_in_student_page" id="search_in_student_page" placeholder="Search..."></input>
                         <button type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
-                    </p>
+                        </div></p>
                 </form>
             </div>
             </body>
