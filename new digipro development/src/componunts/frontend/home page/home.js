cf02c0807589175cb28317f1dacbfe74b545f3c7
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 18:16:51 +0530

    siva Tuesday 31 January 2023 06:16:51 PM IST

--- new digipro development/src/componunts/frontend/home page/home.js
+++ new digipro development/src/componunts/frontend/home page/home.js
@@ -23,7 +23,7 @@ function Home() {
             {/* displaying header in home page */}
             <Header />
 
-            {/* body  */}
+            {/* body tag to show c */}
             <body>
 
                 {/* displaying choose_role page in home page */}
