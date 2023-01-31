c75ccb005406a908acc3000732d242ebf32fbb02
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 18:47:50 +0530

    siva Tuesday 31 January 2023 06:47:50 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -24,7 +24,7 @@ export default function Student_page() {
 
             <div className="input_fields">
                 <p>
-                    <input type="search" name="search_in_student_page" id=
+                    <input type="search" name="search_in_student_page" id="search_"
                 </p>
             </div>
             </body>
