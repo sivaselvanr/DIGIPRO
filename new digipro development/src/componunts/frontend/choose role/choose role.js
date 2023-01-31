a7590bb28ebe0cfa9ff30f988b872d02d7db6fbd
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 11:38:02 +0530

    siva Tuesday 31 January 2023 11:38:02 AM IST

--- new digipro development/src/componunts/frontend/choose role/choose role.js
+++ new digipro development/src/componunts/frontend/choose role/choose role.js
@@ -9,7 +9,7 @@ import "./choose role.css";
 //choose role function
 export default function Choose_role() {
     let navigate = useNavigate();
-    const rout
+    const route
 
     return (
         <div className="choose_role">
