869d4d7d187b0664db82c4877d0f776126819e2f
sivaselvanr <sivaselvancoc5@gmail.com>
2023-01-31 11:17:42 +0530

    siva Tuesday 31 January 2023 11:17:42 AM IST

--- new digipro development/src/componunts/frontend/home page/home.js
+++ new digipro development/src/componunts/frontend/home page/home.js
@@ -31,7 +31,7 @@ function Home() {
 
                 <Router>
                     <Switch>
-                        <Route exact path="/student_page" component
+                        <Route exact path="/student_page" component={}
                     </Switch>
                 </Router>
 
