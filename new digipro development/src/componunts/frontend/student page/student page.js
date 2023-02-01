73b768e4ab872b5e1cae311b96cb761c4ee03e2d
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-02 01:03:08 +0530

    siva Thursday 02 February 2023 01:03:08 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -33,7 +33,7 @@ export default function Student_page() {
                 </form>
             </div>
             
-            {/* displaying StudentDetailsTable in  */}
+            {/* displaying StudentDetailsTable in st */}
             <div className="StudentDetailsTable">
 
                 {/* student details table */}
