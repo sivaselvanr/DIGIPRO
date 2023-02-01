496249975c78b8743272b85630968d1a45a0f4cc
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 11:40:04 +0530

    siva Wednesday 01 February 2023 11:40:04 AM IST

--- new digipro development/src/App.js
+++ new digipro development/src/App.js
@@ -12,7 +12,7 @@ import Student_page from './componunts/frontend/student page/student page';
 function App() {
     return (
       <div>
-        
+        <
         {/* routing to anoter page */}
         <Routes>
 
