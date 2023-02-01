2e30d3d6bfd4fdcc41a5ebc87eafd2105a9cd43b
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 17:41:02 +0530

    siva Wednesday 01 February 2023 05:41:02 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -13,7 +13,7 @@ import BackToTopButton from "../scroll/BackToTopButton";
 export default function Student_page() {
     return (
         <div className="student_page">
-            <div className="HeadAnd"
+            <div className="HeadAndSrea"
             {/* student page heading */}
             <h1 className="student_page_heading">Student Information</h1>
 
