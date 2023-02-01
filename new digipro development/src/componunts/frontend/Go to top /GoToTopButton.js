7f90a913b8602419c766d848439c766e36c00acc
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 12:14:50 +0530

    siva Wednesday 01 February 2023 12:14:50 PM IST

--- new digipro development/src/componunts/frontend/Go to top /GoToTopButton.js
+++ new digipro development/src/componunts/frontend/Go to top /GoToTopButton.js
@@ -37,49 +37,49 @@ const GoToTop = () => {
   );
 };
 
-const Wrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
+// const Wrapper = styled.section`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   position: relative;
 
-  .top-btn {
-    font-size: 2.4rem;
-    width: 6rem;
-    height: 6rem;
-    color: #fff;
-    background-color: ${({ theme }) => theme.colors.btn};
-    box-shadow: ${({ theme }) => theme.colors.shadow};
-    border-radius: 50%;
-    position: fixed;
-    bottom: 5rem;
-    right: 5rem;
-    z-index: 999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
+//   .top-btn {
+//     font-size: 2.4rem;
+//     width: 6rem;
+//     height: 6rem;
+//     color: #fff;
+//     background-color: ${({ theme }) => theme.colors.btn};
+//     box-shadow: ${({ theme }) => theme.colors.shadow};
+//     border-radius: 50%;
+//     position: fixed;
+//     bottom: 5rem;
+//     right: 5rem;
+//     z-index: 999;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     cursor: pointer;
 
-    &--icon {
-      animation: gototop 1.2s linear infinite alternate-reverse;
-    }
+//     &--icon {
+//       animation: gototop 1.2s linear infinite alternate-reverse;
+//     }
 
-    @keyframes gototop {
-      0% {
-        transform: translateY(-0.5rem);
-      }
-      100% {
-        transform: translateY(1rem);
-      }
-    }
-  }
+//     @keyframes gototop {
+//       0% {
+//         transform: translateY(-0.5rem);
+//       }
+//       100% {
+//         transform: translateY(1rem);
+//       }
+//     }
+//   }
 
-  @media (max-width: ${({ theme }) => theme.media.mobile}) {
-    .top-btn {
-      right: 0;
-      left: 40%;
-    }
-  }
-`;
+//   @media (max-width: ${({ theme }) => theme.media.mobile}) {
+//     .top-btn {
+//       right: 0;
+//       left: 40%;
+//     }
+//   }
+// `;
 
 export default GoToTop;
