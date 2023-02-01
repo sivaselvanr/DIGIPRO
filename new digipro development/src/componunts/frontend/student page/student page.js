8ae5a6afb30b98e386470ea82b55a868b24a0a74
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 11:08:46 +0530

    siva Wednesday 01 February 2023 11:08:46 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -50,7 +50,7 @@ export default function Student_page() {
             <h1 className="student_page_heading">Student Information</h1>
 
             {/* Back to top button */}
-            <button
+            {/* <button
             type="button"
             class="btn btn-danger btn-floating btn-lg"
             id="btn-back-to-top"
@@ -59,7 +59,7 @@ export default function Student_page() {
             <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
             </svg>
 
-            </button>
+            </button> */}
                         
             {/* input field */}
             <form>
