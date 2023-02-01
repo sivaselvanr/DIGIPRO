58840df12ee3cd1e38a78792a319fa60ee35e5dc
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 12:58:35 +0530

    siva Wednesday 01 February 2023 12:58:35 PM IST

--- new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
+++ new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
@@ -2,5 +2,5 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function BackToTopButton() {
-    const [backToTopButton, serBack]
+    const [backToTopButton, serBackTop]
 }
