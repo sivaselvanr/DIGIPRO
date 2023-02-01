ef1cf7a23373c9a4d4a3d97babafec6968759292
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:48:11 +0530

    siva Wednesday 01 February 2023 10:48:11 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -9,35 +9,6 @@ import Header from "../header/header";
 //importing footer
 import Footer from "../footer/footer";
 
-//back to top function
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
-
 //student_page function
 export default function Student_page() {
     return (
@@ -45,19 +16,13 @@ export default function Student_page() {
 
             {/* displaying header in student page */}
             <Header />
-
-            {/* Back to top button */}
-            <button
-                type="button"
-                class="btn btn-danger btn-floating btn-lg"
-                id="btn-back-to-top"
-                >
-            {/* <i class="fas fa-arrow-up"></i> */}
-            </button>
             
             {/* student page heading */}
             <h1 className="student_page_heading">Student Information</h1>
                         
+            {/* back to */}
+            <a id="button"></a>
+
             {/* input field */}
             <form>
 
