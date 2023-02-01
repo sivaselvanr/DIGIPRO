74d799d39bafb896a8434d95af51b8d4230fcf57
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 23:02:49 +0530

    siva Wednesday 01 February 2023 11:02:49 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -19,7 +19,11 @@ export default function Student_page() {
                 <h1 className="student_page_heading">Student Information</h1>
                             
                 {/* input field */}
-               
+                <div class="search">
+                          <i class="fa fa-search"></i>
+                          <input type="text" class="form-control" placeholder="Have a question? Ask Now">
+                          <button class="btn btn-primary">Search</button>
+                        </div>
             </div>
             <div className="StudentPageTable">
                 {/* student details table */}
