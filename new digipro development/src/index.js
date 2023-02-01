df351b0152be76d1bcd23260a375d4462e30c077
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:43:56 +0530

    siva Wednesday 01 February 2023 10:43:55 AM IST

--- new digipro development/src/index.js
+++ new digipro development/src/index.js
@@ -10,7 +10,10 @@ root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  return ()
+  return (
+  <div>sdkjfsdklfj;daslkdfj</div>
+
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
