40f36e01dc5853205afddcdd8dad19acd76c5f6f
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-02 00:47:58 +0530

    siva Thursday 02 February 2023 12:47:58 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -19,7 +19,7 @@ export default function Student_page() {
                 <h1 className="StudentPageHeading">Student Information</h1>
                             
                 {/* input field */}
-                <input type="search" id="SearchBox" name="q" 
+                <input type="search" id="SearchBox" name="q"/>
             </div>
 
             <div className="StudentDetailsTable">
