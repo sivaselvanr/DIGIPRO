b32e0b22d7291b52e212a1a33bd61a2d4684fd11
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 13:52:15 +0530

    siva Wednesday 01 February 2023 01:52:15 PM IST

--- new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
+++ new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
@@ -25,7 +25,7 @@ export default function BackToTopButton () {
         <div className="BackToTopButton">
             {backToTopButton && (
                 <button className="BackToTopButton" 
-                onC>
+                onClick=>
                  <i class="bi bi-arrow-up"></i>
                 </button>
             )}
