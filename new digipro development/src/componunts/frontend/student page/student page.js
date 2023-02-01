d3ad2ada277dc05f93f028cefcc799a96cb00db5
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 13:32:49 +0530

    siva Wednesday 01 February 2023 01:32:49 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -3,12 +3,6 @@ import React from "react";
 //importing student_page.css file
 import './student page.css';
 
-//importing header
-import Header from "../header/header";
-
-//importing footer
-import Footer from "../footer/footer";
-
 //student_page function
 export default function Student_page() {
     return (
