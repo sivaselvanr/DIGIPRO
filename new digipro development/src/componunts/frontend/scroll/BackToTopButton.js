3ee50c839c1845c503dba080ff6d0eafdc7db7ab
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 13:13:41 +0530

    siva Wednesday 01 February 2023 01:13:41 PM IST

--- new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
+++ new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
@@ -23,7 +23,7 @@ export default function BackToTopButton () {
 
     return (
         <div className="App">
-            {backToTopButton && (
+            {BackToTopButton && (
                 <button style={{
                     position: "fixed",
                     button: "50px",
