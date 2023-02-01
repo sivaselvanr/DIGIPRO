a3e5bf35c0a236b500ea64a72d20461f4e527d32
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 12:00:38 +0530

    siva Wednesday 01 February 2023 12:00:38 PM IST

--- new digipro development/src/App.js
+++ new digipro development/src/App.js
@@ -15,7 +15,8 @@ import Header from './componunts/frontend/header/header';
 //importing footer
 import Footer from './componunts/frontend/footer/footer';
 
-//importing GoTOTOp
+//importing GoTOTop
+
 
 function App() {
     return (
