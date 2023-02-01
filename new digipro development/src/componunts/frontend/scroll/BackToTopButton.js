c48921a45daf7cfd4f5da89d1956f9cf8f87f5f5
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 13:10:11 +0530

    siva Wednesday 01 February 2023 01:10:11 PM IST

--- new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
+++ new digipro development/src/componunts/frontend/scroll/BackToTopButton.js
@@ -31,7 +31,6 @@ export default function BackToTopButton () {
                     height:"50px",
                     width:"50px",
                     fontSize:"50px",
-
                 }}>^</button>
             )}
         </div>;
