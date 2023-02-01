d75919459f98a51a316168bdc2c616235029813b
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 15:09:20 +0530

    siva Wednesday 01 February 2023 03:09:20 PM IST

--- new digipro development/src/landingpage.js
+++ new digipro development/src/landingpage.js
@@ -9,9 +9,12 @@ export default function Home() {
         <div className="index" >
 
             {/* body tag to show choose role page */}
+            <body>
 
                 {/* displaying choose_role page in home page */}
                 <Choose_role />
+
+            </body>
         </div>
     );
 }
