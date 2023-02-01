52fc3abadd8f53702717240b9c02ade21761ec40
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:32:29 +0530

    siva Wednesday 01 February 2023 10:32:29 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -52,7 +52,7 @@ export default function Student_page() {
                 class="btn btn-danger btn-floating btn-lg"
                 id="btn-back-to-top"
                 >
-            <i class="fas fa-arrow-up"></i>
+            <i class="fas fa-arrow-up">sdf</i>
             </button>
             
             {/* student page heading */}
