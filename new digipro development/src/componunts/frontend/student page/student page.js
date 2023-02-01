4227be3e44b33fc2a780bfa20b41f0e916eab852
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 17:28:50 +0530

    siva Wednesday 01 February 2023 05:28:50 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -4,7 +4,7 @@ import React from "react";
 import './student page.css';
 
 //impriting 
-//importing BackToTop
+//importing BackToTopB
 import BackToTopButton from "../scroll/BackToTopButton";
 
 //student_page function
