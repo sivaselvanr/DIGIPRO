80d2d5b64c8fa7acbfc79d91aceef0f6b3b3b3f6
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:05:56 +0530

    siva Wednesday 01 February 2023 10:05:56 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -22,21 +22,23 @@ export default function Student_page() {
                         
             {/* back to top button */}
             <a id="button"></a>
-            
-            var btn = $('#button');
 
-            $(window).scroll(function() {
-            if ($(window).scrollTop() > 300) {
-                btn.addClass('show');
-            } else {
-                btn.removeClass('show');
-            }
-            });
+           var btn = $('#button');
 
-            btn.on('click', function(e) {
-            e.preventDefault();
-            $('html, body').animate({scrollTop:0}, '300');
-            });
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
 
             {/* input field */}
             <form>
