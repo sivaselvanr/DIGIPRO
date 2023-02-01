0f65ebe57da3c90f71ab6761ec0a5aced1bfb042
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:26:57 +0530

    siva Wednesday 01 February 2023 10:26:57 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -11,30 +11,7 @@ import Footer from "../footer/footer";
 
 //student_page function
 export default function Student_page() {
-    //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    
     return (
         <div className="student_page">
 
