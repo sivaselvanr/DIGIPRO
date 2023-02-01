dd2986b9533aa17c6fd0ac8d4224125954c0246f
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 15:15:13 +0530

    siva Wednesday 01 February 2023 03:15:13 PM IST

--- new digipro development/src/App.js
+++ new digipro development/src/App.js
@@ -21,7 +21,7 @@ function App() {
       <div>
 
         {/* displaying header in all page */}
-        {/* <Header /> */}
+        <Header />
 
         {/* routing to anoter page */}
         <Routes>
