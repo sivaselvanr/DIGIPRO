72fa879c91fe163d7dd90b2a82a71327b2252285
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 15:12:50 +0530

    siva Wednesday 01 February 2023 03:12:50 PM IST

--- new digipro development/src/App.js
+++ new digipro development/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 //importing index from frontend folder
-import Home from './landingpage';
+// import Home from './landingpage';
 
 
 //importing student page from student folder under frontent folder
