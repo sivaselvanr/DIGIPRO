23fb4d9910cf3a3f7606838f83ab2a914bf1786f
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 10:19:00 +0530

    siva Wednesday 01 February 2023 10:19:00 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -17,6 +17,7 @@ export default function Student_page() {
             {/* displaying header in student page */}
             <Header />
             
+            
             {/* student page heading */}
             <h1 className="student_page_heading">Student Information</h1>
                         
