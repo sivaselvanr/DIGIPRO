ecfc0019525e76ca31a6e0db9379c27f04761c80
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-01 12:14:27 +0530

    siva Wednesday 01 February 2023 12:14:27 PM IST

--- new digipro development/src/componunts/frontend/Go to top /GoToTopButton.js
+++ new digipro development/src/componunts/frontend/Go to top /GoToTopButton.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { FaArrowUp } from "react-icons/fa";
 
 const GoToTop = () => {
