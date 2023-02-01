7a9be1e55caac14a087a88a6d84c10652397072b
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-02 00:27:21 +0530

    siva Thursday 02 February 2023 12:27:21 AM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -31,7 +31,7 @@ export default function Student_page() {
                 {/* student details table */}
                 <StudentDetailsTable />
 
-                {/* Button */}
+                {/* BacktButton */}
                 <BackToTopButton />
             </div>
         </div>
