252e25b9687a045ba2607f1855a7309a2bcba340
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-02 19:57:54 +0530

    siva Thursday 02 February 2023 07:57:53 PM IST

--- new digipro development/src/componunts/frontend/student page/StudentDetailsTable.js
+++ new digipro development/src/componunts/frontend/student page/StudentDetailsTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //function to view Student Details in Table View
-export default function StudentDetailsTable(){
+export default function StudentDetailsTable() {
     return(
         <div className="StudentDetailsTable">
             <body>
