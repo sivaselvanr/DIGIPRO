e7fcb1f0664e8b20b855e209c02e208758e21e2c
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-02 20:09:18 +0530

    siva Thursday 02 February 2023 08:09:18 PM IST

--- new digipro development/src/landingpage.js
+++ new digipro development/src/landingpage.js
@@ -3,10 +3,7 @@ import React from "react";
 //importing choose_role page to select role STUDENT or TUTOR
 import Choose_role from "./componunts/frontend/choose role/choose role";
 
-// import StudentDetailsTable './componunts/frontend/student page/StudentDetailsTable'
-import StudentDetailsTable from "./componunts/frontend/student page/StudentDetailsTable";
 
-import ScrollToTop from "./componunts/frontend/scroll/ScrollToTop";
 
 //Home function
 export default function Home() {
