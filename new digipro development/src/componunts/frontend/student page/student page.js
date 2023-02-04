ef00f31757cd203f47737ea4941d7a0a7525757b
sivaselvanr <sivaselvancoc5@gmail.com>
2023-02-04 13:57:25 +0530

    siva Saturday 04 February 2023 01:57:25 PM IST

--- new digipro development/src/componunts/frontend/student page/student page.js
+++ new digipro development/src/componunts/frontend/student page/student page.js
@@ -33,7 +33,7 @@ export default function Student_page() {
                             </svg>
                         </button> */}
                          <div class="input-group">
-                            <input id="SearchBox"type="search" class="form-control" placeholder="Search this blog"/>
+                            <input id="SearchBox" type="search" class="form-control" placeholder="Search this blog"/>
                             <div class="input-group-append">
                                 <button class="btn btn-secondary" type="button">
                                 <i class="fa fa-search"></i>
